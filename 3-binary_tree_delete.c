004bc929f05c03338dff73a15e45d6cab67a18de
Marco Chan <inspiredtolive0@gmail.com>
2019-12-02 12:49:49 -0800

    Adds prototypes and some comments

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Inserts a node as the right-child of another node.
+ * @parent: The pointer to the node to insert the right-child in.
+ * @value: The value to store in the new node.
+ * Return: A pointer to the created node, or NULL.
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+
+}
