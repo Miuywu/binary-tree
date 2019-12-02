b0ba32447fc7aa71e69da434afce09dad702be68
Miuywu <859@holbertonschool.com>
2019-12-02 21:00:06 +0000

    betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,10 +2,12 @@
 /**
  * binary_tree_node - creates a binary tree node
  * Return: binary tree node
+ * @parent: parent of new node
+ * @value: value of new node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t* new;
+	binary_tree_t *new;
 
 	new = malloc(sizeof(binary_tree_t));
 	new->n = value;
